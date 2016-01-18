b2e159e05583dd69b0d3cfad0e0c68bc6f065e0a
Shaun Williams <shaun.e.williams@gmail.com>
2016-01-18 13:31:38 -0600

    add cljsjs parinfer dependency

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "Simpler Lisp Editing"
   :url "https://github.com/shaunlebron/parinfer-cljs"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.228" :scope "provided"]])
+                 [org.clojure/clojurescript "1.7.228" :scope "provided"]
+                 [cljsjs/parinfer "1.4.0-0"]])
 
