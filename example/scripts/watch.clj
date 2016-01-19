0abd3881403b9ae70f94fb28e529a75901b5ff1e
Shaun Williams <shaun.e.williams@gmail.com>
2016-01-19 02:46:47 -0600

    remove example

--- example/scripts/watch.clj
+++
@@ -1,9 +0,0 @@
-(require '[cljs.build.api :as b])
-
-(b/watch "src"
-  {:main 'parinfer-test.core
-   :output-to "out/parinfer_test.js"
-   :output-dir "out"
-   :foreign-libs [{:file "parinfer.js"
-                   :provides ["parinfer"]
-                   :module-type :commonjs}]})
--- example/src/parinfer_test/core.cljs
+++
@@ -1,9 +0,0 @@
-(ns parinfer-test.core
-  (:require
-    [parinfer-cljs.core :refer [indent-mode paren-mode]]))
-
-(enable-console-print!)
-
-(println "Hello world!")
-(println (:text (indent-mode "(foo\nbar)")))
-(println (:text (paren-mode "(foo\nbar)")))
