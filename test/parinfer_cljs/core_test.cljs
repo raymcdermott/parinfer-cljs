7ea6ada772fde3f9fc6ff9773ccf04b8408d3541
ray.mcdermott <ray@mcdermott.be>
2018-04-02 16:09:39 +0200

    Tests were being passed incorrect options (was harmless but still wrong)

--- test/parinfer_cljs/core_test.cljs
+++ test/parinfer_cljs/core_test.cljs
@@ -9,16 +9,13 @@
 
 (deftest happy-path-no-options
   (testing "indent-mode"
-    (is (= indent-result
-           (parinfer/indent-mode "(+ 1 2)" parinfer/js->cljs-output-map))))
+    (is (= indent-result (parinfer/indent-mode "(+ 1 2)"))))
 
   (testing "paren-mode"
-    (is (= indent-result
-           (parinfer/paren-mode "(+ 1 2)" parinfer/js->cljs-output-map))))
+    (is (= indent-result (parinfer/paren-mode "(+ 1 2)"))))
 
   (testing "smart-mode"
-    (is (= indent-result
-           (parinfer/smart-mode "(+ 1 2)" parinfer/js->cljs-output-map)))))
+    (is (= indent-result (parinfer/smart-mode "(+ 1 2)")))))
 
 
 (def unclosed-paren-result
@@ -29,16 +26,13 @@
 
 (deftest unbalanced-no-options
   (testing "indent-mode"
-    (is (= indent-result
-           (parinfer/indent-mode "(+ 1 2" parinfer/js->cljs-output-map))))
+    (is (= indent-result (parinfer/indent-mode "(+ 1 2"))))
 
   (testing "paren-mode"
-    (is (= unclosed-paren-result
-           (parinfer/paren-mode "(+ 1 2" parinfer/js->cljs-output-map))))
+    (is (= unclosed-paren-result (parinfer/paren-mode "(+ 1 2"))))
 
   (testing "smart-mode"
-    (is (= indent-result
-           (parinfer/smart-mode "(+ 1 2" parinfer/js->cljs-output-map)))))
+    (is (= indent-result (parinfer/smart-mode "(+ 1 2")))))
 
 (def unclosed-result
   {:text  "(+ \"1 2)"
@@ -48,16 +42,13 @@
 
 (deftest unclosed-quote-no-options
   (testing "indent-mode"
-    (is (= unclosed-result
-           (parinfer/indent-mode "(+ \"1 2)" parinfer/js->cljs-output-map))))
+    (is (= unclosed-result (parinfer/indent-mode "(+ \"1 2)"))))
 
   (testing "paren-mode"
-    (is (= unclosed-result
-           (parinfer/paren-mode "(+ \"1 2)" parinfer/js->cljs-output-map))))
+    (is (= unclosed-result (parinfer/paren-mode "(+ \"1 2)"))))
 
   (testing "smart-mode"
-    (is (= unclosed-result
-           (parinfer/smart-mode "(+ \"1 2)" parinfer/js->cljs-output-map)))))
+    (is (= unclosed-result (parinfer/smart-mode "(+ \"1 2)")))))
 
 ; :name :eol-backslash, :message "Line cannot end in a hanging backslash."
 
@@ -69,16 +60,13 @@
 
 (deftest eol-backslash-no-options
   (testing "indent-mode"
-    (is (= eol-backslash-result
-           (parinfer/indent-mode "(+ 1 2)\\" parinfer/js->cljs-output-map))))
+    (is (= eol-backslash-result (parinfer/indent-mode "(+ 1 2)\\"))))
 
   (testing "paren-mode"
-    (is (= eol-backslash-result
-           (parinfer/paren-mode "(+ 1 2)\\" parinfer/js->cljs-output-map))))
+    (is (= eol-backslash-result (parinfer/paren-mode "(+ 1 2)\\"))))
 
   (testing "smart-mode"
-    (is (= eol-backslash-result
-           (parinfer/smart-mode "(+ 1 2)\\" parinfer/js->cljs-output-map)))))
+    (is (= eol-backslash-result (parinfer/smart-mode "(+ 1 2)\\")))))
 
 
 (def unmatched-close-paren-fixed-result
@@ -95,16 +83,13 @@
 
 (deftest unmatched-close-paren-test
   (testing "indent-mode"
-    (is (= unmatched-close-paren-fixed-result
-           (parinfer/indent-mode "(let [x 1)" parinfer/js->cljs-output-map))))
+    (is (= unmatched-close-paren-fixed-result (parinfer/indent-mode "(let [x 1)"))))
 
   (testing "paren-mode"
-    (is (= unmatched-close-paren-result
-           (parinfer/paren-mode "(let [x 1)" parinfer/js->cljs-output-map))))
+    (is (= unmatched-close-paren-result (parinfer/paren-mode "(let [x 1)"))))
 
   (testing "smart-mode"
-    (is (= unmatched-close-paren-fixed-result
-           (parinfer/smart-mode "(let [x 1)" parinfer/js->cljs-output-map)))))
+    (is (= unmatched-close-paren-fixed-result (parinfer/smart-mode "(let [x 1)")))))
 
 (def quote-danger-result
   {:text  "(+ 1 2) ; \"bad comment"
@@ -114,16 +99,13 @@
 
 (deftest quote-danger-paren-test
   (testing "indent-mode"
-    (is (= quote-danger-result
-           (parinfer/indent-mode "(+ 1 2) ; \"bad comment" parinfer/js->cljs-output-map))))
+    (is (= quote-danger-result (parinfer/indent-mode "(+ 1 2) ; \"bad comment"))))
 
   (testing "paren-mode"
-    (is (= quote-danger-result
-           (parinfer/paren-mode "(+ 1 2) ; \"bad comment" parinfer/js->cljs-output-map))))
+    (is (= quote-danger-result (parinfer/paren-mode "(+ 1 2) ; \"bad comment"))))
 
   (testing "smart-mode"
-    (is (= quote-danger-result
-           (parinfer/smart-mode "(+ 1 2) ; \"bad comment" parinfer/js->cljs-output-map)))))
+    (is (= quote-danger-result (parinfer/smart-mode "(+ 1 2) ; \"bad comment")))))
 
 (def align-result
   {:success      true
@@ -140,15 +122,64 @@
 
 (deftest realign-test
   (testing "indent-mode"
-    (is (= align-result
-           (parinfer/indent-mode "(let \n[x 1 \ny 2]\n(+ x y))" parinfer/js->cljs-output-map))))
+    (is (= align-result (parinfer/indent-mode "(let \n[x 1 \ny 2]\n(+ x y))"))))
 
   (testing "paren-mode"
-    (is (= align-parens-result
-           (parinfer/paren-mode "(let \n[x 1 \ny 2]\n(+ x y))" parinfer/js->cljs-output-map))))
+    (is (= align-parens-result (parinfer/paren-mode "(let \n[x 1 \ny 2]\n(+ x y))"))))
 
   (testing "smart-mode"
-    (is (= align-result
-           (parinfer/smart-mode "(let \n[x 1 \ny 2]\n(+ x y))" parinfer/js->cljs-output-map)))))
+    (is (= align-result (parinfer/smart-mode "(let \n[x 1 \ny 2]\n(+ x y))")))))
+
+(def cursor-pos-options {:cursor-x 5 :cursor-line 0})
+
+(deftest happy-path-cursor-options
+  (testing "indent-mode"
+    (is (= indent-result (parinfer/indent-mode "(+ 1 2)" cursor-pos-options))))
+
+  (testing "paren-mode"
+    (is (= indent-result (parinfer/paren-mode "(+ 1 2)" cursor-pos-options))))
+
+  (testing "smart-mode"
+    (is (= indent-result (parinfer/smart-mode "(+ 1 2)" cursor-pos-options)))))
 
 
+(def cursor-nl-pos-result
+  {:success      true
+   :text         "(let [x 1]) \n(+ x 2)"
+   :paren-trails [{:line-no 0 :start-x 9 :end-x 11}
+                  {:line-no 1 :start-x 6 :end-x 7}]})
+
+(def cursor-nl-pos-result-paren-mode
+  {:success      true
+   :text         "(let [x 1] \n (+ x 2))"
+   :paren-trails [{:line-no 0 :start-x 9 :end-x 10}
+                  {:line-no 1 :start-x 7 :end-x 9}]})
+
+(def cursor-nl-pos-options {:cursor-x 5 :cursor-line 1})
+
+(deftest happy-path-cursor-nl-options
+  (testing "indent-mode"
+    (is (= cursor-nl-pos-result (parinfer/indent-mode "(let [x 1] \n(+ x 2))" cursor-nl-pos-options))))
+
+  (testing "paren-mode"
+    (is (= cursor-nl-pos-result-paren-mode (parinfer/paren-mode "(let [x 1] \n(+ x 2))" cursor-nl-pos-options))))
+
+  (testing "smart-mode"
+    (is (= cursor-nl-pos-result (parinfer/smart-mode "(let [x 1] \n(+ x 2))" cursor-nl-pos-options)))))
+
+(def fix-result {:success      true
+                 :text         "(def foo [a b])"
+                 :paren-trails [{:line-no 0 :start-x 13 :end-x 15}]})
+
+(def fix-paren {:text  "(def foo [a b"
+                :error {:name :unclosed-paren :message "Unclosed open-paren." :line-no 0 :x 9}})
+
+(deftest happy-path-fix-unmatched
+  (testing "indent-mode"
+    (is (= fix-result (parinfer/indent-mode "(def foo [a b"))))
+
+  (testing "paren-mode"
+    (is (= fix-paren (parinfer/paren-mode "(def foo [a b"))))
+
+  (testing "smart-mode"
+    (is (= fix-result (parinfer/smart-mode "(def foo [a b")))))
